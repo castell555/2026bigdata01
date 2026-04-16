54024e833644580d4990fe3b458915948794e991
castell <castell@email.com>
2026-04-16 15:41:21 +0900

    week07 numpy v0.3.1

--- week07.py
+++ week07.py
@@ -1,21 +1,26 @@
-import random
+# 3) 넘파이 배열 연산
 import numpy as np
+import random
 
-l1 = [9, '짬뽕', 3.7]
-array02 = np.array(l1)
+# l1 = [9, '짬뽕', 3.7, [1, 2, 3]]  # 정수, 문자열, 실수, 리스트
+# l1 = [9, '짬뽕', 3.7]  # 정수, 문자열, 실수
+# array01 = np.array(l1)
+# print(l1)
+# print(array01)
+#
+array02 = np.arange(1, 10)
 print(array02)
-
-array03 = np.ones((2, 4), dtype=int)
-print(array03)
-print(array03.T)
+#
+# array03 = np.ones((2, 4), dtype=int)
+# print(array03)
+# print(array03.T)
 
 l2 = list()
-array04 = np.random.rand(2, 3)
-print(array04)
-print(array04.transpose())
-
+# array04 = np.random.rand(2, 3)
+# print(array04)
+# print(array04.transpose())
 l3 = []
-for j in range(3):
+for i in range(3):
     l2.append(random.random())
 print(l2)
 
@@ -27,9 +32,10 @@ array04 = np.array(l2)
 print(array04 * 10)
 print(array04 > 0.5)
 
+# 4) 넘파이 배열 주요 통계함수
 print(np.mean(array02))
 print(np.median(array02))
 print(np.max(array02))
 print(np.min(array02))
 print(np.var(array02))
-print(np.std(array02)) #넘파이 연산
+print(np.std(array02))
