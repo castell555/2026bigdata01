c2f203c1fd306fac54a3b76611376bfc891d4b8a
castell <castell@email.com>
2026-05-14 16:28:24 +0900

    week11 pandas v1.3

--- week11.py
+++ week11.py
@@ -3,5 +3,10 @@ import seaborn as sns
 ex = sns.load_dataset('exercise')
 # print(ex.head(10))
 # print(ex.info())
-print(ex['kind'].value_counts())
-print(ex['diet'].value_counts())
+# print(ex['kind'].value_counts())
+# print(ex['diet'].value_counts())
+# print(len([ex['diet'] == 'low fat']))
+# print(ex[ex['diet'] == 'low fat'])
+running_df = ex[ex['kind'] == 'running']
+#print(running_df)
+sns.catplot(running_df, x='time', y='pulse', hue='kind', kind='point')
