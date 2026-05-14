314f8858772393ddc16e8a287d702f123b4c2d86
castell <castell@email.com>
2026-05-14 15:35:49 +0900

    week11 pandas v1.2

--- week11.py
+++ week11.py
@@ -1,5 +1,7 @@
 import seaborn as sns
 
-mpg = sns.load_dataset('mpg')
-print(mpg['model_year'].value_counts().sort_values(ascending=False))
-print(mpg.sort_values('model_year',ascending=False))
+ex = sns.load_dataset('exercise')
+# print(ex.head(10))
+# print(ex.info())
+print(ex['kind'].value_counts())
+print(ex['diet'].value_counts())
