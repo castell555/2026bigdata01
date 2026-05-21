8ebfd6a5d61abecc010670e77c290d71089bec51
castell <castell@email.com>
2026-05-21 16:00:44 +0900

    week12 pandas v1.6

--- week12.py
+++ week12.py
@@ -1,9 +1,13 @@
 import seaborn as sns
 
 titanic = sns.load_dataset('titanic')
-#print(titanic.head())
-#print(titanic.describe())
-print(titanic.info())
-# print(titanic['who'])
-# print(titanic['who'].value_counts())
-# print(titanic[['embarked', 'embark_town']])
+#print(titanic[['alive', 'survived', 'pclass', 'class', 'embarked', 'embark_town']])
+titanic01 = titanic.drop(columns=['embarked', 'alive', 'class', 'deck'])
+#print(titanic01.info)
+#print(titanic01['deck01'])
+# print(titanic01['pclass'].value_counts())
+#print(titanic01[titanic01['pclass'] == 3])
+# surivival_rate_pclass = titanic01.groupby('pclass')['survived'].mean()
+# print(surivival_rate_pclass)
+surivival_rate_sex = titanic01.groupby('sex')['survived'].mean() * 100
+print(surivival_rate_sex)
