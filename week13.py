7523a9c8462b0cf9a2759f7f9cbffca8c2dc48e9
castell <castell@email.com>
2026-05-28 15:25:02 +0900

    week13 panda v1.9

--- week13.py
+++ week13.py
@@ -1,11 +1,10 @@
 import seaborn as sns
 # import matplotlib.pyplot as plt
 
+#(데이터 측정)년도, (대상)국가, (1인당 연간 의료비)지출액, (평균)기대수명
 health = sns.load_dataset("healthexp")
-# print(health.head())
-# print(health.tail(7))
-# print(health.info())
-# print(health.describe())
-print(health['Country'].unique())
-print(health['Country'].nunique())
-print(health['Country'].value_counts())
+# 가장 최근 연도인 2020년 데이터를 필터링 후 추출
+# health_2020 = health[health['Year']==2020]
+# print(health_2020)
+# print(health_2020.sort_values(by=['Life_Expectancy'], ascending=False))
+# print(health_2020.sort_values(ascending=False, 'Life_Expectancy'))
